32cdf8b2014398d4b89f0f525aa8abdb98a35ed3
Edwardd02 <122767943+Edwardd02@users.noreply.github.com>
2023-03-22 10:29:34 +0900

    label added

--- app/src/main/java/com/example/assignment05/SelectionActivity.kt
+++ app/src/main/java/com/example/assignment05/SelectionActivity.kt
@@ -2,10 +2,8 @@ package com.example.assignment05
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment04.ImageAdapter
